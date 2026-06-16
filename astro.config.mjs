bedd4159f70fac4020a26d0c43bb12d3781a850d
Name <you@example.com>
2026-06-16 17:38:20 +0800

    chore: add site URL for sitemap and OG

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
 export default defineConfig({
+  site: "https://shaifx.vercel.app",
   integrations: [
     starlight({
       title: "闪光小破站",
